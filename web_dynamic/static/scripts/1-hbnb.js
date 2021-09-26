bf996af7dbe1d3d3b434b028d1032ac6ba0c5230
mareizaa <marce.3210@gmail.com>
2021-09-26 15:26:13 -0500

    script jquery

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,15 @@
 const $ = window.$;
-let amenities = []
+let aid = []
+let aname = []
 $('input[type="checkbox"]').change(function(){
-    if($(this).is('checked')) {
-        amenities.push($(this).attr('data-name'))
-        console.log(amenities);
+    if($(this).is(':checked')) {
+        aid.push($(this).attr('data-id'))
+        aname.push($(this).attr('data-name'))
     } else {
-        amenities.pop('data-id')
+        let indexi = aid.indexOf($(this).attr('data-id'));
+        let indexn = aname.indexOf($(this).attr('data-name'));
+        aid.splice(indexi, 1);
+        aname.splice(indexn, 1);
     }
+    $('.amenities h4').text(aname);
 });
